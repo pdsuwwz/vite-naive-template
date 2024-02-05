ad52ff434bf00ffe19827aaba640273707436563
Wisdom <pdsu.wwz@foxmail.com>
2024-02-05 17:48:43 +0800

    🧃 feat: up sfc script setup for home page

--- auto-imports.d.ts
+++ auto-imports.d.ts
@@ -323,6 +323,7 @@ declare global {
 // for vue template auto import
 import { UnwrapRef } from 'vue'
 declare module 'vue' {
+  interface GlobalComponents {}
   interface ComponentCustomProperties {
     readonly EffectScope: UnwrapRef<typeof import('vue')['EffectScope']>
     readonly _: UnwrapRef<typeof import('lodash-es')>
@@ -632,6 +633,7 @@ declare module 'vue' {
   }
 }
 declare module '@vue/runtime-core' {
+  interface GlobalComponents {}
   interface ComponentCustomProperties {
     readonly EffectScope: UnwrapRef<typeof import('vue')['EffectScope']>
     readonly _: UnwrapRef<typeof import('lodash-es')>
