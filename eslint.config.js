7bc3bb87ab90fc395e3f9635a0b859094fe87b72
Wisdom <pdsu.wwz@foxmail.com>
2024-05-08 14:35:18 +0800

    📝 fix: typo eslint comments

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,5 @@
 // import antfu from '@antfu/eslint-config'
 // export default antfu()
-// import antfu from '@antfu/eslint-config'
-// export default antfu()
 
 import globals from 'globals'
 import { defineFlatConfig } from 'eslint-define-config'
